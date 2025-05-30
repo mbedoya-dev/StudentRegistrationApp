45a41d08fb47edb86cdf3dd5ee2755511e8c6135
Mauricio Bedoya <mauriyi@hotmail.com>
2025-05-30 11:24:30 -0500

    fix(sidebar): corrige texto 'Estudiantes' en los datos del sidebar

--- StudentRegistration.Web/src/app/layouts/full/sidebar/sidebar-data.ts
+++ StudentRegistration.Web/src/app/layouts/full/sidebar/sidebar-data.ts
@@ -2,7 +2,7 @@ import { NavItem } from './nav-item/nav-item';
 
 export const navItems: NavItem[] = [
   {
-    navCap: 'Estuiantes',
+    navCap: 'Estudiantes',
   },
   {
     displayName: 'Registrar Estudiante',
